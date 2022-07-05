4f352504347f26db33e41e8929e66a2fc49b2734
ajankowy97 <agnieszka@thehumanaspect.com>
2022-07-05 07:25:53 +0200

    test - code structure change

---
+++ auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { NotFoundError } from "./errors/not-found-error";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "../src/middlewares/error-handler";
+
+export const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(errorHandler);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
